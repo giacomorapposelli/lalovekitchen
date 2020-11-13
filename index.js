d76b74ec4a6c097d5ab142ac00b04ae885dc7b58
Luca Tomarelli <lucatomarelli1@gmail.com>
2020-11-13 16:04:15 +0100

    ready to start

--- index.js
+++ index.js
@@ -2,37 +2,36 @@ const express = require("express");
 const app = express();
 const compression = require("compression");
 
-
-const cookieSession = require("cookie-session");
-const csurf = require("csurf");
-const { sendEmail } = require("./ses.js");
-const { ElastiCache, ApiGatewayManagementApi } = require("aws-sdk");
-const cryptoRandomString = require("crypto-random-string");
+// const cookieSession = require("cookie-session");
+// const csurf = require("csurf");
+// const { sendEmail } = require("./ses.js");
+// const { ElastiCache, ApiGatewayManagementApi } = require("aws-sdk");
+// const cryptoRandomString = require("crypto-random-string");
 
 app.use(express.static(__dirname + "/public"));
 app.use(compression());
 
-app.use(
-    cookieSession({
-        secret: `I'm always angry.`,
-        maxAge: 1000 * 60 * 60 * 24 * 14,
-    })
-);
+// app.use(
+//     cookieSession({
+//         secret: `I'm always angry.`,
+//         maxAge: 1000 * 60 * 60 * 24 * 14,
+//     })
+// );
 
-app.use(
-    express.urlencoded({
-        extended: false,
-    })
-);
+// app.use(
+//     express.urlencoded({
+//         extended: false,
+//     })
+// );
 
 app.use(express.json());
 
-app.use(csurf());
+// app.use(csurf());
 
-app.use(function (req, res, next) {
-    res.cookie("mytoken", req.csrfToken());
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.cookie("mytoken", req.csrfToken());
+//     next();
+// });
 
 if (process.env.NODE_ENV != "production") {
     app.use(
@@ -45,8 +44,8 @@ if (process.env.NODE_ENV != "production") {
     app.use("/bundle.js", (req, res) => res.sendFile(`${__dirname}/bundle.js`));
 }
 
-app.get("*",function(req,res){
-    res.sendFile(__dirname + "/index.html")
-})
+app.get("*", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
 
 app.listen(process.env.PORT || 8080, () => console.log("server listening"));
